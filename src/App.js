f5b9642a805d9cc6f4c882244854aeb24bb01180
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-02-10 14:34:14 -0800

    dynamic content and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Title from './components/Title'
 import POPOSList from './components/POPOSList';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Title />
       <POPOSList />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <p>Isabella Devassy Copyright 2023</p>
+        </div>
+    )
+}
+
+export default Footer;
