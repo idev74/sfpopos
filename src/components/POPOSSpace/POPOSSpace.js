46d737ca1786220a87ca75731576a85d0643ffd9
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-04-18 12:04:29 -0700

    altered alt text

--- src/components/POPOSSpace/POPOSSpace.js
+++ src/components/POPOSSpace/POPOSSpace.js
@@ -14,7 +14,7 @@ export default function POPOSSpace(props) {
             <img src={`${process.env.PUBLIC_URL}/images/${image}`} 
             width="300" 
             height="300" 
-            alt="public space" 
+            alt={`clickable view of ${name}`} 
             />
             </Link>
             <section className='POPOSSpace-info'>
